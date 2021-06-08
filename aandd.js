8d7f21bbe189dcd7285b2d4eeae01035a03e8570
cramakick138 <cramakick138@gmail.com>
2021-06-08 17:10:48 +0900

    all commit 4

--- aandd.js
+++ aandd.js
@@ -1,20 +1,19 @@
-function show(array) {
-
-    if (Array.isArray(array)) {
+function show(obj) {
+    if (Array.isArray(obj)) {
         var html = '';
         html = html + '<table border="1">';
         html = html + '<tr>';
-        for (var i = 0; i < array.length; i++) {
-            html = html + '<td>' + array[i] + '</td>';
+        for (var i = 0; i < obj.length; i++) {
+            html = html + '<td>' + obj[i] + '</td>';
         }
         html = html + '</tr>';
         html = html + '</table>';
-        //document.getElementById('res').innerHTML = html;
+        //html = html + '<br>';
         document.getElementById('res').insertAdjacentHTML('beforeend', html);
     } else {
-        var html = JSON.stringify(array);
-        html = html + '<br>';
-        document.getElementById('res').insertAdjacentHTML('beforeend', html);
+        var string = JSON.stringify(obj);
+        string = string + '<br>';
+        document.getElementById('res').insertAdjacentHTML('beforeend', string);
     }
 }
 
@@ -28,5 +27,18 @@ function createRandomNumbers(array, n) {
     for (i = 0; i < n; i++) {
         var r = Math.floor(Math.random() * 10)
         array.push(r + 1);
+    }
+}
+
+function createOrderedRandomNumbers(numbers, n) {
+    var count = 0;
+    for (var i = 1; ; i++) {
+        if (Math.floor(Math.random() * 3) == 0) {
+            numbers.push(i);
+            count++;
+            if (count >= n) {
+                break;
+            }
+        }
     }
 }
